b265530c8648c762074fd34c9cd99075679cabb2
maazwaheed123 <167740222+maazwaheed123@users.noreply.github.com>
2024-07-31 12:48:38 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -13,10 +13,10 @@ app.use(cors());
 
 app.use("/api/images", imageRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client" , "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client" , "index.html"));
 });
 
 // MongoDB connection
